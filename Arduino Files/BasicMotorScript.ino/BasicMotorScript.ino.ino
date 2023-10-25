ca33357a1424eef46be2b4004554eda4bcfd9504
mgrzybek2005 <mgrzybek@uoguelph.ca>
2023-10-25 15:06:42 -0400

    Documentation

--- Arduino Files/BasicMotorScript.ino/BasicMotorScript.ino.ino
+++ Arduino Files/BasicMotorScript.ino/BasicMotorScript.ino.ino
@@ -1,59 +1,61 @@
-// Motor A
+// Motor A Pin Declarations
 int enA = 13;
 int in1A = 12;
 int in2A = 11;
-// Motor B
+
+// MOTOR B UNOPERATIONALE FOR PROTOTYPE
+// Motor B Pin Declarations
 // int enB = 10;
 // int in1B = 9;
 // int in2B = 8;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
 
+  // Motor A Pin Setup
   pinMode(enA, OUTPUT);
   pinMode(in1A, OUTPUT);
   pinMode(in2A, OUTPUT);
 
+  // Motor B Pin Setup
   // pinMode(enB, OUTPUT);
   // pinMode(in1B, OUTPUT);
   // pinMode(in2B, OUTPUT);
 
-
+  // Enabling Both Motors
   digitalWrite(enA, HIGH);
   // digitalWrite(enB, LOW);
 
+  // Show that Serial is active
   Serial.println("Begin");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // Set pins to move forward
   digitalWrite(in1A, HIGH);
   digitalWrite(in2A, LOW);
   Serial.println("A: HIGH LOW.");
 
   delay(3000);
 
+  // Stop
   digitalWrite(in1A, LOW);
   digitalWrite(in2A, LOW);
 
   delay(3000);
 
+  // Set pins to move backward
   digitalWrite(in1A, LOW);
   digitalWrite(in2A, HIGH);
   Serial.println("A: LOW HIGH.");
 
   delay(3000);
-
+  
+  // Stop
   digitalWrite(in1A, LOW);
   digitalWrite(in2A, LOW);
 
   delay(3000);
-
-  // digitalWrite(in1B, LOW);
-  // digitalWrite(in2B, LOW);
-  // Serial.println("B: LOW LOW.");
-
-
 }
